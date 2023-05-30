2b2269ab4945ba7719d83d53e9b13cd065296988
dex <jwug0821@gmail.com>
2023-05-30 16:25:53 +0900

    facade class for dividing responsibility

--- src/main/java/com/example/stock/facade/LettuceLockStockFacade.java
+++ src/main/java/com/example/stock/facade/LettuceLockStockFacade.java
@@ -1,2 +1,26 @@
-package com.example.stock.facade;public class LettucLockStockFacede {
+package com.example.stock.facade;
+
+import com.example.stock.repository.RedisLockRepository;
+import com.example.stock.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class LettuceLockStockFacade {
+    private final RedisLockRepository redisLockRepository;
+    private final StockService stockService;
+
+    @Transactional
+    public void decrease(Long key, Long quantity) throws InterruptedException {
+        while(!redisLockRepository.lock(key)){
+            Thread.sleep(100);
+        }
+        try{
+            stockService.decrease(key, quantity);
+        } finally {
+            redisLockRepository.unlock(key);
+        }
+    }
 }
--- src/main/java/com/example/stock/facade/OptimisticLockFacade.java
+++ src/main/java/com/example/stock/facade/OptimisticLockFacade.java
@@ -1,2 +1,22 @@
-package com.example.stock.facade;public class OptimisticLockFacade {
+package com.example.stock.facade;
+
+import com.example.stock.service.OptimisticLockStockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OptimisticLockFacade {
+    private final OptimisticLockStockService optimisticLockStockService;
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        while(true){
+            try{
+                optimisticLockStockService.decrease(id, quantity);
+                break;
+            }   catch(Exception e){
+                Thread.sleep(50);
+            }
+        }
+    }
 }
